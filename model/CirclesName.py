1f7ddab059432e2dd3664e07fcbfbf2fa30a284b
SHE21 <santiagolenil@gmail.com>
2024-12-07 16:58:59 -0300

    created classe enum cricleName

---
+++ model/CirclesName.py
@@ -0,0 +1,10 @@
+import enum
+
+
+class CirclesName(enum):
+    GESTATION = "gestation"
+    MATERNITY = "maternity"
+    CRECHE = "creche"
+    CRESCIMENTO = "crescimento"
+    ENGORDA = "engorda"
+    ABATE = "abate"
